314a4bdd9a87c743a0b929b85906699bf0361b16
csh0034 <csh0034@gmail.com>
2023-01-05 17:16:55 +0900

    feat: init

---
+++ src/main/java/com/ask/travis/TravisCiPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.ask.travis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisCiPracticeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TravisCiPracticeApplication.class, args);
+  }
+
+}
