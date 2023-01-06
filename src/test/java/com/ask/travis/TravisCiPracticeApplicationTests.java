335ee5955b379e3ee8b90256b427d716d5ee114e
csh0034 <csh0034@gmail.com>
2023-01-06 14:15:59 +0900

    test: crash

--- src/test/java/com/ask/travis/TravisCiPracticeApplicationTests.java
+++ src/test/java/com/ask/travis/TravisCiPracticeApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @Slf4j
-class TravisCiPracticeApplicationTests {
+class TravisCiPracticeA pplicationTests {
 
   @Test
   void contextLoads() {
